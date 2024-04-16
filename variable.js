c873b1340a977128d89985763a561aa31ce23a52
Arbath Abdurrahman <arbath21@gmail.com>
2024-04-16 20:47:17 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,9 @@
+let firstName; //statement
+firstName = "Arbath"; //expression
+
+console.log(firstName);
+// let bersifat open (bisa dirubah)
+
+const lastName = "Abdurrahman";
+console.log(lastName);
+//const bersifat lock (tidak bisa dirubah)
