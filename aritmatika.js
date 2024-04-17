24b2f322846196f4226e110e2c00870f4ae5ce1e
Arbath Abdurrahman <arbath21@gmail.com>
2024-04-17 19:55:38 +0700

    aritmatika

---
+++ aritmatika.js
@@ -0,0 +1,22 @@
+let x = 10;
+let y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(y % x); //modulus
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == y);
+
+//increment
+let post = 10;
+console.log(post++); //expression akan menghasilkan nilai variable sebelum ditingkatkan nilainya
+console.log(post); //meghasilkan output 11
+
+//decrement
+let pre = 5;
+console.log(++pre); //expression akan menghasilkan nilai variable setelah ditingkatkan nilainya
