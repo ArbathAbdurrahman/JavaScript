4f0759618e23c7ec0e0628bddc0952fe1aead9f2
Arbath Abdurrahman <arbath21@gmail.com>
2024-04-22 08:11:22 +0700

    pengkondisian Switch Case

---
+++ SwitchCase.js
@@ -0,0 +1,15 @@
+//Switch
+let bahasa = "arab";
+let me = null; //variable kosong tp bernilai
+switch (bahasa) {
+  case "jawa":
+    me = "kula";
+    break;
+  case "indonesia":
+    me = "saya";
+    break;
+  case "arab":
+    me = "ana";
+    break;
+}
+console.log(me); //menampilkan ke console
