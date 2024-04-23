58b819fe93886248eaafaf2a5ec634d2ace46fc7
Arbath Abdurrahman <arbath21@gmail.com>
2024-04-23 08:21:43 +0700

    Perulangan

---
+++ loop.js
@@ -0,0 +1,25 @@
+//perulangan nilai 1 sampai 5 (inisiasi variabel ; test kondisi ; perubahan nilai variabel)
+//for
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+//for of loop
+let myFriend = ["Ridho", "Fauzan", "Ahmad", "Abid"];
+for (const arrayItem of myFriend) {
+  console.log(arrayItem);
+}
+
+//while
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+//do-while
+let b = 1;
+do {
+  console.log(b);
+  b++;
+} while (b <= 5);
