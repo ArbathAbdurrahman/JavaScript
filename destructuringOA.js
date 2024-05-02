1e3aa423d1ea7d33ec560cf4e3f264055fb6da0c
Arbath Abdurrahman <arbath21@gmail.com>
2024-05-02 13:40:17 +0700

    menambahkan beberapa kondisi

--- destructuringOA.js
+++ destructuringOA.js
@@ -25,3 +25,57 @@ let destructuring = [
 ];
 
 console.log(destructuring);
+
+// destructuring Objek
+const profile = {
+  firstName: "Ucup",
+  lastName: "Supriyadi",
+  age: 20,
+};
+
+const { firstName, lastName, age } = profile;
+console.log(firstName, lastName, age);
+//  assignment
+let pirstName = "Dimas";
+let umur = 20;
+
+// menginisialisasi nilai baru melalui destructuring object
+({ pirstName, umur } = profile);
+console.log(pirstName);
+console.log(umur);
+
+//  mengatasi undefined
+const profilee = {
+  awalNameName: "John",
+  lastName: "Doe",
+  usia: 18,
+};
+const { awalName, usia, isMale = false } = profilee;
+
+console.log(awalName);
+console.log(usia);
+console.log(isMale);
+
+// destructuring Array
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+
+const [firstFood, secondFood, thirdFood, fourthFood] = favorites;
+
+console.log(firstFood);
+console.log(secondFood);
+console.log(thirdFood);
+console.log(fourthFood);
+
+// memanggil array tertentu
+const [, , tirdFood] = favorites;
+
+console.log(tirdFood);
+
+// assignment
+let myFood = "Ice Cream";
+let herFood = "Noodles";
+
+[myFood, herFood] = favorites;
+
+console.log(myFood);
+console.log(herFood);
